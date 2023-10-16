75090218901e36aa8e576291fc229baca0f6bcb4
Roy Geesing <roy.geesing@students.fhnw.ch>
2023-10-16 15:22:06 +0200

    a lot of changes

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -5,14 +5,43 @@ public class Ball : MonoBehaviour
     private Vector3 _v = new(0, 0, 0);
     private readonly Vector3 _a = new(0, -9.81f, 0);
 
+    private const float RotationSpeed = 20f;
+    private readonly Vector3 _rotateUp = new(RotationSpeed, 0, 0);
+    private readonly Vector3 _rotateLeft = new(0, 0, RotationSpeed);
+
+    public Collider plane;
+
     private void FixedUpdate()
     {
         transform.position += Time.deltaTime * _v + 0.5f * Time.deltaTime * Time.deltaTime * _a;
         _v += _a * Time.deltaTime;
-    }
 
-    private void OnCollisionEnter(Collision other)
-    {
-        _v = Vector3.Reflect(_v, other.contacts[0].normal);
+        var normal = plane.transform.up;
+        var vector = plane.transform.position - transform.position;
+        var distanceVector = Vector3.Project(vector, normal);
+        var distance = distanceVector.magnitude;
+        if (distance < 0.5f)
+        {
+            _v = Vector3.Reflect(_v, normal);
+        }
+
+        var up = Time.deltaTime * _rotateUp;
+        var left = Time.deltaTime * _rotateLeft;
+        if (Input.GetKey("up") || Input.GetKey("w"))
+        {
+            plane.transform.Rotate(up);
+        }
+        if (Input.GetKey("down") || Input.GetKey("s"))
+        {
+            plane.transform.Rotate(-up);
+        }
+        if (Input.GetKey("left") || Input.GetKey("a"))
+        {
+            plane.transform.Rotate(left);
+        }
+        if (Input.GetKey("right") || Input.GetKey("d"))
+        {
+            plane.transform.Rotate(-left);
+        }
     }
 }
---
+++ Assets/sphere.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    private Vector3 _v = new(20, 0, 0);
+    private readonly Vector3 _center = new(0, 0, 0);
+    
+    private void FixedUpdate()
+    {
+        var thisTransform = transform;
+        var position = thisTransform.position;
+        var halfTime = Time.deltaTime / 2;
+        var vMagnitudeSquared = _v.magnitude * _v.magnitude;
+        
+        var positionBetween = position + halfTime * _v;
+        
+        var inwards = _center - positionBetween;
+        var inwardsForce = vMagnitudeSquared / inwards.magnitude;
+        var acceleration = inwards.normalized * inwardsForce;
+        _v += acceleration * Time.deltaTime;
+        thisTransform.position = positionBetween + halfTime * _v;
+    }
+}
